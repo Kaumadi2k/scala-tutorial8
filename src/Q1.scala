fd57b64b47877f0d3306f6d0b2f18cc58fc9f160
Kaumadi2k <136589603+Kaumadi2k@users.noreply.github.com>
2023-08-13 18:08:39 +0530

    Update Q1.scala

--- src/Q1.scala
+++ src/Q1.scala
@@ -8,9 +8,3 @@ object Q1 {
     println(interest(5000))
   }
 }
-
-/*Develop the function of interest. It consumes a deposit amount and produces
-the actual amount of interest that the money earns in a year. The bank pays a
-flat 2% for deposits of up to Rs. 20 000, a flat 4% per year for deposits of up to
-Rs. 200000, a flat 3.5% per year for deposits of up to Rs. 2000000, and a flat
-6.5% for deposits of more than Rs. 200000. (Hint: Use Lambda functions)*/
